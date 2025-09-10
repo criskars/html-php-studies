75d32a888fa4adc849eab0d5bafc3e6b786431b7
criskars <crisale.karsten@gmail.com>
2025-09-10 16:16:00 -0300

    youtube api studies

--- Youtube API/unidade01/js/meuscript.js
+++ Youtube API/unidade01/js/meuscript.js
@@ -5,27 +5,36 @@ fetch('../key.json')
         $(document).ready(function () {
             $.get("https://www.googleapis.com/youtube/v3/channels", {
                 part: "contentDetails",
-                forUsername: "eloycasagrandee",
+                forUsername: "QuadrinhosNaSarjeta",
                 key: YoutubeAPIKey
             },
                 function (data) {
                     $.each(data.items, (i, item) => {
-                        uploads_id = item.contentDetails.relatedPlaylists.uploads;
+                        const uploads_id = item.contentDetails.relatedPlaylists.uploads;
                         getUploads(uploads_id);
                     })
                 })
-            function getUploads(uploadsPlaylistId) {
+            function getUploads(uploadsPlaylistId, pageToken = "") {
                 $.get("https://www.googleapis.com/youtube/v3/playlistItems", {
                     part: "snippet",
                     maxResults: 50,
                     playlistId: uploadsPlaylistId,
+                    pageToken: pageToken,
                     key: YoutubeAPIKey
                 },
                     function (data) {
                         $.each(data.items, (i, item) => {
-                            videoTitle = item.snippet.title;
-                            videoId = item.snippet.resourceId.videoId;
-                            videoThumbnail = item.snippet.thumbnails.maxres.url;
+                            let videoTitle = item.snippet.title;
+                            let videoId = item.snippet.resourceId.videoId;
+                            let publishedAt = new Date(item.snippet.publishedAt);
+                            let description = item.snippet.description;
+                            console.log(item.snippet);
+                            let formattedDate = publishedAt.toLocaleDateString('pt-BR', {
+                                day: '2-digit',
+                                month: '2-digit',
+                                year: 'numeric'
+                            });
+                            let videoThumbnail = item.snippet.thumbnails.maxres?.url || item.snippet.thumbnails.standard?.url || item.snippet.thumbnails.high?.url || item.snippet.thumbnails.medium?.url || item.snippet.thumbnails.default?.url;
                             $("#janela").append(`
 
                             <div class="card mb-2 shadow">
@@ -36,18 +45,25 @@ fetch('../key.json')
                                         <p class="card-text">${videoTitle}</p>
                                     </div>
                                 </div>
-                                <div class="card-body">
-                                    `
-                                    + `<a href="https://www.youtube.com/watch?v=${videoId}" target="_blank" class="btn btn-primary">Assistir</a>` +
-                                `</div>
+                                <div class="card-body d-flex flex-row align-items-center justify-content-between">
+                                    <p class="card-text mb-0">Publicado em: ${formattedDate}</p>
+                                    <a href="https://www.youtube.com/watch?v=${videoId}" target="_blank" class="btn btn-primary">Assistir</a>
+                                    
+                                </div>
+                                <div class="card-footer">
+                                    <p class="card-text mb-0">Descrição: ${description}</p>
+                                </div>
                             </div>
 
                     `);
                         })
+                        if (data.nextPageToken) {
+                            getUploads(uploadsPlaylistId, data.nextPageToken);
+                        }
                     }
                 )
-            }
 
+            }
         })
     })
     .catch(error => {
