4e328666d70eb9105a8edaa8de811bdfc6646a5f
Cris <crisale.karsten@gmail.com>
2025-09-15 18:36:09 -0300

    SEO studies

--- Youtube API/unidade03/js/meuscript.js
+++ Youtube API/unidade03/js/meuscript.js
@@ -1,10 +1,18 @@
-$(document).ready(function() {
-    $.get("", {
-            part: '',
-            id: '',
-            key: 'AIzaSyB49WfTkgfK2menTbmVCkLG0f9cYWQ9XKU'},
-            function(data) {
-                console.log(data);
-            }
-    )
-});
+fetch("../key.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const YoutubeAPIKey = data.YouTubeAPIKey;
+    $(document).ready(function () {
+      $.get(
+        "https://www.googleapis.com/youtube/v3/videos",
+        {
+          part: "statistics",
+          id: "loUxtxfps1k",
+          key: YoutubeAPIKey,
+        },
+        function (data) {
+          console.log(data);
+        }
+      );
+    });
+  });
