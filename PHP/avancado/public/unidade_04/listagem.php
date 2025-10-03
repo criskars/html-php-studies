cfb5f6f0211b24236b7ca888fff8af188590e26f
criskars <crisale.karsten@gmail.com>
2025-10-03 15:17:01 -0300

    PHP studies

--- PHP/avancado/public/unidade_04/listagem.php
+++ PHP/avancado/public/unidade_04/listagem.php
@@ -1,29 +1,51 @@
-<?php require_once("../../conexao/conexao.php"); ?>
+<?php require_once("../../conexao/conexao.php");
+// Consulta ao banco de dados
+$produtos = "SELECT produtoID, nomeproduto, tempoentrega, precounitario, imagempequena FROM produtos";
+$lista_produtos = mysqli_query($con, $produtos);
+if (!$lista_produtos) {
+    die("Falha na consulta ao banco de dados");
+}
+
+?>
 
 <!doctype html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Curso PHP Integração com MySQL</title>
-        
-        <!-- estilo -->
-        <link href="_css/estilo.css" rel="stylesheet">
-    </head>
 
-    <body>
-        <?php include_once("../_incluir/topo.php"); ?>
-        <?php include_once("../_incluir/funcoes.php"); ?>
-        
-        <main>  
-            
-        </main>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso PHP Integração com MySQL</title>
 
-        <?php include_once("../_incluir/rodape.php"); ?> 
-    </body>
+    <!-- estilo -->
+    <link href="_css/estilo.css" rel="stylesheet">
+    <link href="_css/produtos.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php include_once("../_incluir/topo.php"); ?>
+    <?php include_once("../_incluir/funcoes.php"); ?>
+
+    <main>
+        <div id="listagem_produtos">
+            <ul>
+                <?php while ($linha = mysqli_fetch_assoc($lista_produtos)) {
+                    echo "<li class='imagem'>";
+                    echo "<img src=" . $linha["imagempequena"] . "></li>";
+                    echo "<li><h3>" . $linha["nomeproduto"] . "</h3></li>";
+                    echo "<li>" . $linha["tempoentrega"] . " dias úteis </li>";
+                    echo "<li>R$ " .  number_format($linha["precounitario"], 2, ",", ".") . "</li>";
+                }
+                ?>
+            </ul>
+        </div>
+    </main>
+
+    <?php include_once("../_incluir/rodape.php"); ?>
+</body>
+
 </html>
 
 <?php
-    // Fechar conexao
-    mysqli_close($conecta);
+// Fechar conexao
+mysqli_close($con);
 ?>
