d6fb150616c36e46bf0f7e603a6672c4ab532d8a
criskars <crisale.karsten@gmail.com>
2025-11-13 14:40:55 -0300

    PHP studies

--- PHP/php_ajax/unidade_12/exercicio_02.php
+++ PHP/php_ajax/unidade_12/exercicio_02.php
@@ -1,87 +1,92 @@
 <!doctype html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>PHP com AJAX</title> 
-        
-        <link href="_css/estilo.css" rel="stylesheet">
-    </head>
 
-    <body>
-        <main>  
-            <div id="janela_formulario">
-                
-                <form id="formulario_transportadora">
-                    <label for="nometransportadora">Nome da Transportadora</label>
-                    <input type="text" value="" name="nometransportadora" id="nometransportadora">
-                    
-                    <label for="cep">CEP (ex: 58000-100)</label>
-                    <input type="text" value="" name="cep" id="cep" maxlength="9">
-                    
-                    <label for="endereco">Endereço</label>
-                    <input type="text" value="" name="endereco" id="endereco">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP com AJAX</title>
 
-                    <label for="cidade">Cidade</label>
-                    <input type="text" value="" name="cidade" id="cidade">
+    <link href="_css/estilo.css" rel="stylesheet">
+</head>
 
-                    <label for="estado">Estado</label>
-                    <input type="text" value="" name="estado" id="estado">
+<body>
+    <main>
+        <div id="janela_formulario">
 
-                    <label for="bairro">Bairro</label>
-                    <input type="text" value="" name="bairro" id="bairro">
-                    
-                    <input type="submit" value="Confirmar alteração">  
-                    
-                    <div id="mensagem">
-                        <p></p>
-                    </div>
-                </form> 
-                
-            </div>
-        </main>
-        
-        <script src="_js/jquery-3.7.1.min.js"></script>
-        <script>
-            $('#cep').on('blur', function() {
-                var cep = $(this).val().replace(/\D/g, '');
-                if (cep !== "") {
-                    var validacep = /^[0-9]{8}$/;
-                    if(validacep.test(cep)) {
-                        $.ajax({
-                            url: 'https://viacep.com.br/ws/' + cep + '/json/',
-                            type: 'GET',
-                            dataType: 'json',
-                            success: function(data) {
-                                if (!("erro" in data)) {
-                                    $('#endereco').val(data.logradouro);
-                                    $('#bairro').val(data.bairro);
-                                    $('#cidade').val(data.localidade);
-                                    $('#estado').val(data.uf);
-                                } else {
-                                    limparFormulario();
-                                    alert("CEP não encontrado.");
-                                }
-                            },
-                            error: function() {
+            <form id="formulario_transportadora">
+                <label for="nometransportadora">Nome da Transportadora</label>
+                <input type="text" value="" name="nometransportadora" id="nometransportadora">
+
+                <label for="cep">CEP (ex: 58000-100)</label>
+                <input type="text" value="" name="cep" id="cep" maxlength="9">
+
+                <label for="endereco">Endereço</label>
+                <input type="text" value="" name="endereco" id="endereco">
+
+                <label for="cidade">Cidade</label>
+                <input type="text" value="" name="cidade" id="cidade">
+
+                <label for="estado">Estado</label>
+                <input type="text" value="" name="estado" id="estado">
+
+                <label for="bairro">Bairro</label>
+                <input type="text" value="" name="bairro" id="bairro">
+
+                <input type="submit" value="Confirmar alteração">
+
+                <div id="mensagem">
+                    <p></p>
+                </div>
+            </form>
+
+        </div>
+    </main>
+
+    <script src="_js/jquery-3.7.1.min.js"></script>
+    <script>
+        $('#cep').on('blur', function() {
+            var cep = $(this).val().replace(/\D/g, '');
+            if (cep !== "") {
+                var validacep = /^[0-9]{8}$/;
+                if (validacep.test(cep)) {
+                    $.ajax({
+                        url: 'https://viacep.com.br/ws/' + cep + '/json/',
+                        type: 'GET',
+                        dataType: 'json',
+                        success: function(data) {
+                            if (!("erro" in data)) {
+                                $('#endereco').val(data.logradouro);
+                                $('#bairro').val(data.bairro);
+                                $('#cidade').val(data.localidade);
+                                $('#estado').val(data.uf);
+                            } else {
                                 limparFormulario();
-                                alert("Erro ao consultar o CEP.");
+                                $("#mensagem").show();
+                                $("#mensagem p").text("CEP não encontrado.");
                             }
-                        });
-                    } else {
-                        limparFormulario();
-                        alert("Formato de CEP inválido.");
-                    }
+                        },
+                        error: function() {
+                            limparFormulario();
+                            $("#mensagem").show();
+                            $("#mensagem p").text("Erro ao consultar o CEP.");
+                        }
+                    });
                 } else {
                     limparFormulario();
+                    $("#mensagem").show();
+                    $("#mensagem p").text("Formato de CEP inválido.");
                 }
-            });
-
-            function limparFormulario() {
-                $('#endereco').val("");
-                $('#bairro').val("");
-                $('#cidade').val("");
-                $('#estado').val("");
+            } else {
+                limparFormulario();
             }
-        </script>
-    </body>
+        });
+
+        function limparFormulario() {
+            $('#endereco').val("");
+            $('#bairro').val("");
+            $('#cidade').val("");
+            $('#estado').val("");
+        }
+    </script>
+</body>
+
 </html>
--- PHP/php_ajax/unidade_12/exercicio_04.php
+++ PHP/php_ajax/unidade_12/exercicio_04.php
@@ -1,14 +1,38 @@
 <!doctype html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>PHP com AJAX</title>
-    </head>
 
-    <body>
+<head>
+    <meta charset="UTF-8">
+    <title>PHP com AJAX</title>
+</head>
 
-        <script src="_js/jquery-3.7.1.min.js"></script>
-        <script>
-        </script>
-    </body>
+<body>
+    <div id="resultado"></div>
+    <script src="_js/jquery-3.7.1.min.js"></script>
+    <script>
+        $.ajax({
+            url: "https://api.open-meteo.com/v1/forecast",
+            type: 'POST',
+            data: {
+                latitude: -26.4861,
+                longitude: -49.0667,
+                hourly: "temperature_2m",
+                timezone: "America/Sao_Paulo"
+            },
+            success: function(response) {
+                var temperatureData = [];
+                $.each(response.hourly.temperature_2m, function(index, value) {
+                    temperatureData.push(value);
+                });
+                $.each(response.hourly.time, function(index, value) {
+                    $("#resultado").append("Hora " + value + ": " + temperatureData[index] + "°C<br>");
+                });
+            },
+            error: function(xhr, status, error) {
+                alert('Erro na requisição: ' + error);
+            }
+        })
+    </script>
+</body>
+
 </html>
